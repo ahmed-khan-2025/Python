ea40b2781ac531fbbb05fcf3e842c19cb921bb61
Ahmed <ahmed.kahn@gmail.com>
2025-04-09 16:08:48 +0200

    changes

--- basic/Flask/simple.py
+++ basic/Flask/simple.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
+    return render_template("index.html")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return render_template('result.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
